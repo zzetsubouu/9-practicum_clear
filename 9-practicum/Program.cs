6fe961518d3219c759b028945c9ab12c9b03a184
User <User@WIN-N8HGK237C16>
2024-11-04 23:32:13 +0200

    Добавьте файлы проекта.

---
+++ 9-practicum/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_practicum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1.Определить, имеются ли в одномерном массиве случайных чисел элементы, значения которых кратны 11.
+            //Вывести такие значения.
+            //const int value = 5;
+            //int[] ints = new int[value];
+            //for (int i = 0; i < ints.Length; i++)
+            //{
+            //    Console.Write($"inst[{i}]= ");
+            //    ints[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("числа массива, кратные 11:");
+            //foreach (int element in ints)
+            //{
+            //    if (element % 11 == 0)
+            //    {
+            //        Console.WriteLine(element);
+            //    }
+            //}
+
+
+
+            //Задача 2.Найти сумму и количество положительных элементов в одномерном массиве с явной инициализацией.
+            //int[] massiv = { 3, 5, 8, -7, -5, 10, 11, -2 };
+            //int count = 0, sum = 0;
+            //foreach (var element in massiv)
+            //{
+            //    if (element > 0)
+            //    {
+            //        sum += element;
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine($"Количество положительных чисел в массиве: {count}\nСумма положительных чисел массива: {sum}");
+
+
+
+            //Задача 3. Вывести элементы одномерного массива, заполненного с клавиатуры ,в обратном порядке.
+            int[] invertedMas = new int[5];
+            for (int i = 0; i < invertedMas.Length; i++)
+            {
+                Console.Write($"inst[{i}]= ");
+                invertedMas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Перевернутый массив:");
+            for (int j = invertedMas.Length - 1; j >= 0; j--)
+            {
+                Console.WriteLine(invertedMas[j]);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 9-practicum/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("9-practicum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("9-practicum")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("fd2f4dee-5dce-4220-8d28-c9fc10c64d97")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
